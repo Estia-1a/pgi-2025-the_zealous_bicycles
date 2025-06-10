46edd2731e1dde997a97be5583dd3b23e82d66d1
Nonoxcab22 <noah.cabarry@etu.estia.fr>
2025-06-10 11:28:30 +0200

    Ajout de la fonction 2

--- src/features.h
+++ src/features.h
@@ -4,5 +4,7 @@
 
 void helloWorld();
 void dimension (char *source_path);
+void first_pixel (char *source_path);
+
 
 #endif
--- src/main.c
+++ src/main.c
@@ -36,6 +36,7 @@ int main(int argc, char **argv) {
    
     dimension(configuration.filenames[0]);
   }
+
   /*
    * TO COMPLETE
    */
