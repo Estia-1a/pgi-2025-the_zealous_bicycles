d7c0b2f6893fb688cde59b94a18e009f683e2298
alexdad94 <alexandre.daguebert@etu.estia.fr>
2025-06-19 23:06:00 +0200

    scale_crop

--- src/features.h
+++ src/features.h
@@ -46,7 +46,7 @@ void mirror_vertical(char *source_path);
 
 void mirror_total(char *source_path);
 
-
+void scale_crop(char *source_path, int center_x, int center_y, int crop_width, int crop_height);
 
 void scale_nearest(char *source_path, float coeff);
 
