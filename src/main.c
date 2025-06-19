7db2f34eefe1db687d126a02944b9b02ada667d2
alexdad94 <alexandre.daguebert@etu.estia.fr>
2025-06-19 19:07:55 +0200

    write_image dans vertical

--- src/main.c
+++ src/main.c
@@ -176,7 +176,7 @@ else if ( strncmp( configuration.command, "scale_nearest", 13) == 0 ) {
 }
 
 else if ( strcmp( configuration.command, "color_desaturate") == 0 ) {
-  /* color_gray_luminance() function is defined in feature.h and implemented in feature.c */
+  /* color_desaturate() function is defined in feature.h and implemented in feature.c */
   
   color_desaturate(configuration.filenames[0]);
 }
